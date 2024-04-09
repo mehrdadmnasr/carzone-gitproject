7aa1ed9b1b499b5d916710e9b3cbe968efda44d7
mehrdadmnasr <mehrdad.m.nasr@gmail.com>
2024-04-09 10:18:44 +0330

    Completed Team models

--- carzone/settings.py
+++ carzone/settings.py
@@ -78,7 +78,10 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'carzone_db',
+        'USER': 'postgres',
+        'PASSWORD' : '1682168',
+        'HOST' : 'localhost',
     }
 }
 
@@ -128,3 +131,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Media settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
